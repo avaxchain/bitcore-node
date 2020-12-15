99e424ca1a6abb6daeeeb85ff74c84ab71723290
avaxchain <68371916+avaxchain@users.noreply.github.com>
2020-12-15 22:49:37 +0100

    Update installExplorer.sh

--- installExplorer.sh
+++ installExplorer.sh
@@ -10,7 +10,7 @@ sudo apt-get install -y libzmq3-dev
 
 # MongoDB
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.1 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.1.list
+echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.listsudo apt-get update
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 sudo systemctl enable mongod
